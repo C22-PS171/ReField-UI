aae9502e5a2b586fa0ca783268d99383ab397645
lazuardh <habiyanlazuard21@gmail.com>
2022-06-05 17:29:23 +0700

    update login

---
+++ app/src/main/java/com/bangkit/myfield/ui/home/HomeLogin.kt
@@ -0,0 +1,12 @@
+package com.bangkit.myfield.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.myfield.R
+
+class HomeLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_login)
+    }
+}
