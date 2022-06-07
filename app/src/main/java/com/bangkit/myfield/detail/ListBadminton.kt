9f11886761bc884ae07b4ceaec6616b4135efb5f
lazuardh <habiyanlazuard21@gmail.com>
2022-06-08 00:03:14 +0700

    add search in activity LisBadminton

--- app/src/main/java/com/bangkit/myfield/detail/ListBadminton.kt
+++ app/src/main/java/com/bangkit/myfield/detail/ListBadminton.kt
@@ -8,5 +8,7 @@ class ListBadminton : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_badminton)
+
+        supportActionBar?.hide()
     }
 }
